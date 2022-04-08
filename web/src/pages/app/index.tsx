b27c009b5ca631b2eb2427ab03226545a654d732
Mateus Garcia <mpgxc@pop-os.localdomain>
2022-04-08 03:36:58 -0300

    feat: added web

---
+++ web/src/pages/app/index.tsx
@@ -0,0 +1,19 @@
+import {
+  useUser,
+  UserProvider,
+  withPageAuthRequired,
+} from "@auth0/nextjs-auth0";
+
+function Home() {
+  const { user } = useUser();
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+    </div>
+  );
+}
+
+export const getServerSideProps = withPageAuthRequired();
+export default Home;
---
+++ web/src/pages/index.tsx
@@ -0,0 +1,25 @@
+import { getSession } from "@auth0/nextjs-auth0";
+
+export default function Home() {
+  return null;
+}
+
+export const getServerSideProps = async ({ req, res }) => {
+  const session = getSession(req, res);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/api/auth/login",
+        permanent: false,
+      },
+    };
+  } else {
+    return {
+      redirect: {
+        destination: "/app",
+        permanent: false,
+      },
+    };
+  }
+};
