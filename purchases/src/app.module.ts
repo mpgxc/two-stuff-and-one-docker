afb7df12ea64db6f62ba209fc7fee274736fab79
mpgxc <mpgx5.c@gmail.com>
2022-04-10 19:54:44 -0300

    refactor: adjust entry points

--- purchases/src/app.module.ts
+++ purchases/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+
 import { InfraModule } from './infra/infra.module';
 @Module({
   imports: [InfraModule],
--- purchases/src/infra/http/http.module.ts
+++ purchases/src/infra/http/http.module.ts
@@ -1,13 +1,37 @@
-import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+
+import path from 'path';
+import { CustomersService } from 'services/customers.service';
+import { ProductsService } from 'services/products.service';
+import { PurchasesService } from 'services/purchases.service';
+
+import { CustomersResolver } from './graphql/resolvers/customers.resolver';
+import { ProductsResolver } from './graphql/resolvers/products.resolver';
+import { PurchaseResolver } from './graphql/resolvers/purchases.resolver';
+import { AuthorizationGuard } from './guards/authorization.guard';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: true,
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
+  ],
+  providers: [
+    //Services
+    AuthorizationGuard,
+    ProductsService,
+    PurchasesService,
+    CustomersService,
+
+    //Resolvers
+    ProductsResolver,
+    PurchaseResolver,
+    CustomersResolver,
   ],
 })
 export class HttpModule {}
