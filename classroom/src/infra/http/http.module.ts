6b9fcddb6e5bcf0cb8949eb3682397976b4aa97e
mpgxc <mpgx5.c@gmail.com>
2022-04-11 19:40:59 -0300

    feat: inject deps

--- classroom/src/infra/http/http.module.ts
+++ classroom/src/infra/http/http.module.ts
@@ -1,13 +1,35 @@
-import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+
+import path from 'path';
+import { CoursesService } from 'services/courses.service';
+import { EnrollmentsService } from 'services/enrollments.service';
+import { StudentsService } from 'services/students.service';
+
+import { CoursesResolver } from './graphql/resolvers/courses.resolver';
+import { EnrollmentsResolver } from './graphql/resolvers/enrollments.resolver';
+import { StudentsResolver } from './graphql/resolvers/students.resolver';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: true,
+      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
     }),
+  ],
+  providers: [
+    //Resovlers
+    CoursesResolver,
+    StudentsResolver,
+    EnrollmentsResolver,
+
+    //Services
+    CoursesService,
+    EnrollmentsService,
+    StudentsService,
   ],
 })
 export class HttpModule {}
--- classroom/src/infra/infra.module.ts
+++ classroom/src/infra/infra.module.ts
@@ -1,14 +1,11 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AuthorizationGuard } from './http/guards/authorization.guard';
+import { Global, Module } from '@nestjs/common';
+
 import { DatabasesModule } from './databases/databases.module';
 import { HttpModule } from './http/http.module';
-import { AuthResolver } from 'auth/auth.controller';
 
+@Global()
 @Module({
-  providers: [AuthorizationGuard, AuthResolver],
-  imports: [ConfigModule.forRoot(), DatabasesModule, HttpModule],
-  exports: [],
-  controllers: [],
+  imports: [DatabasesModule, HttpModule],
+  exports: [DatabasesModule],
 })
 export class InfraModule {}
