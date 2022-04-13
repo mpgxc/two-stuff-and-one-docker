14dbeab3d9d7bd45a64acf3521d3d504e24800ff
mpgxc <mpgx5.c@gmail.com>
2022-04-13 20:35:39 -0300

    feat: added kafka service

--- purchases/src/infra/infra.module.ts
+++ purchases/src/infra/infra.module.ts
@@ -2,10 +2,11 @@ import { Global, Module } from '@nestjs/common';
 
 import { DatabasesModule } from './databases/databases.module';
 import { HttpModule } from './http/http.module';
+import { MessagingModule } from './messaging/messaging.module';
 
 @Global()
 @Module({
-  imports: [DatabasesModule, HttpModule],
-  exports: [DatabasesModule],
+  imports: [DatabasesModule, HttpModule, MessagingModule],
+  exports: [DatabasesModule, MessagingModule],
 })
 export class InfraModule {}
