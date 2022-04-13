5fd83aaadb61625216c178579d0db6522a27b0c6
mpgxc <mpgx5.c@gmail.com>
2022-04-13 04:54:34 -0300

    feat: added entry point to call producers

--- classroom/src/main.ts
+++ classroom/src/main.ts
@@ -1,9 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.APP_PORT || 3000);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'classroom',
+        brokers: [process.env.KAFKA_BROKERS as string],
+      },
+      consumer: {
+        groupId: 'purchases',
+      },
+    },
+  });
+
+  app.startAllMicroservices().then(() => {
+    console.log('[MICROSERVICE] is running!');
+  });
+
+  app.listen(process.env.APP_PORT || 3000).then(() => {
+    console.log('[HTTP/GRAPHQL] is running!');
+  });
 }
 bootstrap();
